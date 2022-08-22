3b413d4d330ddb4d93b1c94c00989090fff59ec6
Julio6968 <111807958+Julio6968@users.noreply.github.com>
2022-08-22 18:02:05 -0500

    Add files via upload

---
+++ entrada_datos.m
@@ -0,0 +1,9 @@
+clear all
+close all 
+clc
+x = input("dame una dato x ");
+y = input("Dame un dato y");
+z = x + y ;
+w = x*y;
+disp({"la suma de datos es ", x})
+disp({"la multiplicacion de datos es ", w})
---
+++ scrip_1_variables.m
@@ -0,0 +1,3 @@
+x = 2.1421i % mi variable x
+y = 5.14i
+z = x + y 
